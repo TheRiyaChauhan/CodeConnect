c67757e95120977d52398a20ca48433dfdf2f8c6
Riya Chauhan <hiriyachauhan123@gmail.com>
2025-06-22 19:16:17 +0530

    created login and signup api and authentication middleware

---
+++ src/middlewares/auth.js
@@ -0,0 +1,32 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+
+const userAuth = async (req, res, next) => {
+    try{
+        const {token} = req.cookies;
+
+        if(!token){
+            throw new Error("No token provided");
+        }
+
+        const decodeObject =await jwt.verify(token, 'your_jwt_secret');
+
+        const {_id} = decodeObject;
+
+        const user = await User.findOne({_id});
+
+        if(!user){
+           throw new Error("User not found");
+        }
+
+        req.user = user;
+
+        next();
+
+    }
+    catch(err){
+        res.status(401).send("Unauthorized: " + err.message);
+    }
+}
+
+module.exports = userAuth;
--- src/models/User.js
+++ src/models/User.js
@@ -1,25 +1,60 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const userSchema = new mongoose.Schema({
     firstName:{
         type: String,
+        required: true,
+        minlength: 4,
+        maxlength: 20,
     },
     lastName: {
         type: String,
     },
     emailId:{
         type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+        validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("Invalid email address: " + value);
+        }
+      },
     },
     password: {
         type: String,
+        required: true,
     },
     age: {
         type: Number
     },
     gender:{
         type: String,
-    }
-});
+        enum: {
+        values: ["male", "female", "other"],
+        message: `{VALUE} is not a valid gender type`,
+        },
+    },
+    photoUrl: {
+        type: String,   
+        default:"https://miro.medium.com/v2/resize:fit:1024/1*gQzkQ3uJ0SwJL51t16bivw.jpeg",
+        validate(value) {
+        if (!validator.isURL(value)) {
+          throw new Error("Invalid Photo URL: " + value);
+        }
+      }
+    },
+    about: {
+        type: String,
+        default: "Hey there! I am using CodeConnect, a platform to connect with developers and share knowledge."
+    },
+    skills: {
+        type: [String]
+    },
+
+},{timestamps: true});
 
 const userModel = mongoose.model('User', userSchema);
 
