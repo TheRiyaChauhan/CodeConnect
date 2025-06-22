e5ed0408664ca74e9af7b9b1e29b60bd99e2c7c0
Riya Chauhan <hiriyachauhan123@gmail.com>
2025-06-23 00:27:42 +0530

    created Api's for viewing and editing profile

--- Server/src/routes/profile.js
+++ Server/src/routes/profile.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const userAuth = require('../middlewares/auth'); 
 const User = require('../models/User'); 
+const { validateEditProfileData } = require('../utils/validator'); 
 
 const profileRouter = express.Router();
 
@@ -12,6 +13,29 @@ profileRouter.get('/profile/view', userAuth, async (req, res) => {
         res.status(200).send(user);
     }catch(err){
         return res.status(500).send("Error fetching profile: " + err.message);
+    }
+})
+
+profileRouter.patch('/profile/edit', userAuth, async (req, res) => {
+    try{
+        if(!validateEditProfileData(req)) {
+            return res.status(400).send("Invalid fields for profile edit");
+        }
+
+        const loggedInUser = req.user; // User is set by the userAuth middleware
+
+        Object.keys(req.body).forEach((key)=>{ loggedInUser[key] = req.body[key] });
+
+        await loggedInUser.save();
+
+        res.json({
+            message: `${loggedInUser.firstName} Profile updated successfully`,
+            data: loggedInUser
+        });
+
+    }
+    catch(err){
+        return res.status(500).send("Error updating profile: " + err.message);
     }
 })
 
