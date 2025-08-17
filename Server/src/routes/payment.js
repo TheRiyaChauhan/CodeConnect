c2efecf8af566b1c99d45786f7a12c623eb96fc9
Riya Chauhan <hiriyachauhan123@gmail.com>
2025-08-17 18:04:24 +0530

    bug

--- Server/src/routes/payment.js
+++ Server/src/routes/payment.js
@@ -62,7 +62,7 @@ paymentRouter.post("/payment/webhook", async (req, res) => {
     try {
 
         console.log("Webhook Called");
-        const webhookSignature = req.get("X-Razorpay-Signature");
+        const webhookSignature = req.headers["X-Razorpay-Signature"];
         console.log("Webhook Signature", webhookSignature);
 
         const isWebhookValid = validateWebhookSignature(
